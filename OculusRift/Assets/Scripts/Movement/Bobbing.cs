e6fb59f668b0da34fd5bfd46bb3a8105eb3070d5
Daan Ruiter <daanruiter@live.nl>
2015-05-27 08:43:23 +0200

    sync

---
+++ OculusRift/Assets/Scripts/Movement/Bobbing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bobbing : MonoBehaviour {
+    public enum BobbingDirection
+    {
+        None,
+        Up,
+        Down
+    }
+
+    public float maxBobbing;
+    public float minBobbing;
+    public float bobbingSpeed;
+
+    private float m_currentBobbing;
+    private BobbingDirection m_direction;
+
+    private void Start()
+    {
+        m_direction = BobbingDirection.Up;
+    }
+
+    private void Update()
+    {
+
+    }
+}
+
